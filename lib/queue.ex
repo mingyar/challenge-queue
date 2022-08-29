311729212a26e62187fc79f2e49389fcd21c2335
Mingyar Furtado <mingyar.furtado@gmail.com>
2022-08-29 00:03:00 -0300

    Solution

--- lib/queue.ex
+++ lib/queue.ex
@@ -1,18 +1,47 @@
 defmodule Queue do
-  @moduledoc """
-  Documentation for `Queue`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  # Client
 
-  ## Examples
+  def start_link(initial_queue) when is_list(initial_queue) do
+    GenServer.start_link(__MODULE__, initial_queue)
+  end
 
-      iex> Queue.hello()
-      :world
+  def enqueue(pid, element) do
+    GenServer.cast(pid, {:enqueue, element})
+  end
 
-  """
-  def hello do
-    :world
+  def dequeue(pid) do
+    GenServer.call(pid, :dequeue)
+  end
+
+  # Server (Callbacks)
+  @impl true
+  def init(queue) do
+    {:ok, queue}
+  end
+
+  @impl true
+  # sync
+  def handle_call({:enqueue, element}, _from, queue) do
+    new_queue = queue ++ [element]
+
+    {:reply, new_queue, new_queue}
+  end
+
+  @impl true
+  def handle_call(:dequeue, _from, [head | tail]) do
+    {:reply, head, tail}
+  end
+
+  @impl true
+  def handle_call(:dequeue, _from, []) do
+    {:reply, nil, []}
+  end
+
+  @impl true
+  # async
+  def handle_cast({:enqueue, element}, queue) do
+    {:noreply, queue ++ [element]}
   end
 end
